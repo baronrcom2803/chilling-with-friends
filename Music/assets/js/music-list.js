557c1f536680ee228039285a288cdf3a228a7f39
baronrcom2803 <rcomdat28.3@gmail.com>
2023-03-18 22:20:11 +0700

    Initial commit

---
+++ Music/assets/js/music-list.js
@@ -0,0 +1,68 @@
+let allMusic = [
+    {
+        name:"BlueTequila",
+        artist: "Táo",
+        img: "baron.png",
+        src: "music-1"
+    },
+    {
+        name:"thế giới có nơi nào cho em",
+        artist: "Unknown",
+        img: "baron.png",
+        src: "music-2"
+    },
+    {
+        name:"Ngủ sớm đi em",
+        artist: "DucMinh",
+        img:"baron.png",
+        src: "y2mate.com - Ngủ sớm đi em  DucMinh  Prod by GC"
+    },
+    {
+        name:"Mai Về",
+        artist: "Dfoxie37 x Myhai",
+        img:"baron.png",
+        src: "y2mate.com - MAI VỀ  Dfoxie37 x Myhai  37 SOUND Official Music Video"
+    },
+    {
+        name:"Thôi hẹn em",
+        artist: "Madihu",
+        img:"baron.png",
+        src: "y2mate.com - Madihu  Thôi hẹn em Official Music Video"
+    },
+    {
+        name:"Có em",
+        artist: "Madihu ft LowG",
+        img: "baron.png",
+        src: "y2mate.com - Madihu  Có em Feat Low G Official MV"
+    },
+    {
+        name:"Nếu lúc đó",
+        artist: "tlinh",
+        img: "baron.png",
+        src: "Y2Mate.is - tlinh - nếu lúc đó (ft. 2pillz)  OFFICIAL MUSIC VIDEO-fyMgBQioTLo-160k-1655454159086"
+    },
+    {
+        name:"Thôi em đừng đi",
+        artist: "RPT MCK",
+        img: "baron.png",
+        src: "Y2Mate.is - 07. Thôi Em Đừng Đi - RPT MCK ( ft. Trung Trần )   99  the album-6fzH7a3XQW0-160k-1654363438957"
+    },
+    {
+        name:"Anh đã ổn hơn",
+        artist: "RPT MCK",
+        img: "baron.png",
+        src: "Y2Mate.is - 14. Anh Đã Ổn Hơn - RPT MCK  99 the album-dz6xe0xXqYE-160k-1656441265402"
+    },
+    {
+        name:"Dear",
+        artist: "Gonzo x Lê Hiếu",
+        img: "baron.png",
+        src: "Y2Mate.is - GONZO x Lê Hiếu  Dear (Official MV)-kSRTQv1qEno-160k-1655469901348"
+    },
+    {
+        name:"Em không khóc",
+        artist: "buitruonglinh ft vuphungtien",
+        img: "baron.png",
+        src: "y2mate.com - Em Không Khóc  buitruonglinh ft vuphungtien"
+    }
+];
